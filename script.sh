6d79841b0d306c9237be611fe7e4f66ba0559fe1
Mridul Gupta <mridulg@users.noreply.github.com>
2016-11-28 08:55:38 +0530

    update script till consul (1)

--- script.sh
+++ script.sh
@@ -11,3 +11,17 @@ docker-machine env consul
 
 # Run this command to configure your shell: 
 eval $(docker-machine env consul)
+
+# We will store the private IP of this host as KV_IP environment variable with the following command.
+export KV_IP=$(docker-machine ssh consul 'ifconfig eth0 | grep "inet addr:" | cut -d: -f2 | cut -d" " -f1')
+
+# Print it just for the sake of it
+echo $KV_IP
+
+# We need to connect out docker client to this host and then run progrium/consul image there.
+# This command will pull and deploy the image in consul host.
+docker run -d \
+  -p ${KV_IP}:8500:8500 \
+  -h consul \
+  --restart always \
+  gliderlabs/consul-server -bootstrap
